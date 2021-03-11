681afc166ee77f5db40267a374c2a25996469b2e
Coaltic <glindsay156@gmail.com>
2021-03-11 09:05:42 -0400

    Initial commit

---
+++ Assets/UIManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public float score;
+    public Text scoreOutput;
+
+    public AudioSource sadCat;
+    public AudioSource cat;
+    public AudioSource gangstaCat;
+
+    public GameObject gangstaGlasses;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreOutput.text = score.ToString();
+    }
+
+    public void IncreaseScore()
+    {
+        score += 10;
+        cat.Play();
+    }
+
+    public void resetScore()
+    {
+        score = 0;
+        gangstaGlasses.SetActive(false);
+        sadCat.Play();
+    }
+
+    public void newGangstaCat()
+    {
+        gangstaGlasses.SetActive(true);
+        score = 0;
+        gangstaCat.Play();
+    }
+    
+}
